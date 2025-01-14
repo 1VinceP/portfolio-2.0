863182816ebce3820586b18ae8ffc968de81903d
1VinceP <vpalmergraphics@gmail.com>
2025-01-14 14:03:36 -0600

    add scroll-to-top on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import $styles from './app.module.css';
+import { useEffect } from "react";
 
 function App() {
+   const location = useLocation();
+
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, [location]);
+
    return (
       <div className={$styles.app}>
          <Header />
